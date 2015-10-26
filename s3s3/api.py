e77042c914b9725da0fef7e56ede12635c1a876b
J. Matt Peterson <jmatt@jmatt.org>
2015-10-26 13:26:16 -0700

    Fix typo. dest_key => dest_keys.
    	modified:   s3s3/api.py

--- s3s3/api.py
+++ s3s3/api.py
@@ -15,11 +15,12 @@ def create_connection(connection_args):
 def upload(source_key, dest_keys):
     """
     `source_key` The source boto s3 key.
-    `dest_keys` The destination boto s3 keys.
+    `dest_keys` A list of the destination boto s3 keys.
     """
     # Use the same name if no destination key is passed.
-    if not dest_key:
-        dest_key = source_key
+    if not dest_keys or not source_key:
+        raise Exception(
+            'The source_key and dest_keys parameters are required.')
     with tempfile.NamedTemporaryFile() as data:
         source_key.get_contents_to_file(data)
         for dest_key in dest_keys:
